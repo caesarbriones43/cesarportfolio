3bf12249d24518d8af284b352af8c0a9b384151a
caesarbriones43 <caesarbriones43@gmail.com>
2023-10-14 09:49:58 -0600

    002

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -12,7 +12,7 @@ export function Welcome() {
       {/* <Title className={classes.title} ta="center" mt={100}>
         Welcome to{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+          Mantineasdasd
         </Text>
       </Title> */}
 
